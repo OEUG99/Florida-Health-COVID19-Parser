6597ea7e774b128f4ad4b7f9312e2328fa1b263d
OEUG99 <iam@oweneugenio.com>
2021-10-20 18:26:41 -0400

    Added an example for fetching data from the PDF's tables.

---
+++ examples/fetch_data_from_table.py
@@ -0,0 +1,19 @@
+import CovidFL
+
+
+def main():
+    # Creating a parser object.
+    parser = CovidFL.Parser()
+
+    # Using our parser object to fetch data from the PDF's tables:
+    table_data_dictionary = parser.fetch_table_data()
+
+    # Fetching just the 'Vaccine age groups' table as a dictionary:
+    vaccine_age_groups_data = table_data_dictionary['vaccine-age-groups']
+
+    # Printing the dictionary contain "Vaccine age groups" data:
+    print(vaccine_age_groups_data)
+
+
+if __name__ == "__main__":
+    main()
