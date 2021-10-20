2855408df5dd47bb99ad0ff6b3657ddd15c18b27
OEUG99 <iam@oweneugenio.com>
2021-10-20 18:28:23 -0400

    Added a setup file, to easily use the module.

--- main.py
+++ main.py
@@ -1,75 +1,23 @@
-import urllib
-import PyPDF2
-from urllib.request import urlopen
-from io import BytesIO
-import urllib.error
+import CovidFL
 
-DATA_URL = 'http://ww11.doh.state.fl.us/comm/_partners/covid19_report_archive/covid19-data/covid19_data_latest.pdf'
-
-
-def open_url(url):
-    """ Create a byte stream for the provided URL.
-    This is like `open(filename, 'rb')` except, we don't need to have the file locally stored, we just
-    grab the bytes via a get-request to the URL.
-
-    :param url: The URL we wish to pull the file from.
-    :return: `io.BytesIO()`. A stream containing the bytes from the file that is located at the URL.
-    """
-
-    http_response = ''
-    try:
-        http_response = urllib.request.urlopen(url)
-    except urllib.error.URLError:
-        Exception('COVID-PDF: Error fetching the URL')
-
-    return BytesIO(http_response.read())
-
-
-def str_to_int(array):
-    """  Converts an array of comma seperated number-strings into a usable integer array.
-
-    :param array: str array. Converts comma seperated number-strings into usable integers.
-    :return: int array.
-    """
-    corrected_array = []
-
-    for x in array:
-        # Converting str to int then adding it to the array.
-        corrected_array.append(int(x.replace(',', '')))
-
-    return corrected_array
-
-
-def print_data(array):
-    """ Print function that displays the weekly COVID-19 statistics for Florida.
-
-    :param array: An array of ints. Represents the weekly covid-19 stats. Index 1 = weekly numbers, index2 = cumulative.
-    :return: None
-    """
-    print('==Total Covid-19 Cases==')
-    print(f'Previous Week: {array[0]}')
-    print(f'Cumulative: {array[1]}')
 
 
 def main():
-    # Getting our file via the provided URL.
-    file = open_url(DATA_URL)
+    # Creating a parser object.
+    parser = CovidFL.Parser()
 
-    # Creating a PDF reader object:
-    pdfReader = PyPDF2.PdfFileReader(file)
+    # Using our parser object to fetch data from the PDF's tables:
+    table_data_dictionary = parser.fetch_table_data()
 
-    # Creating a page object based off the first page, then extracting the raw text:
-    pageObj = pdfReader.getPage(0)
-    raw_text = pageObj.extractText().split('\n') # Splitting our string at every new line into an array of strings.
+    # Fetching just the 'Vaccine age groups' table as a dictionary:
+    vaccine_age_groups_data = table_data_dictionary['vaccine-age-groups']
 
-    # Processing the raw text
-    result = str_to_int(raw_text[13:15])
+    # Printing the dictionary contain "Vaccine age groups" data:
+    print(vaccine_age_groups_data)
 
-    # Printing our results:
-    print_data(result)
 
-    # Lastly, we close the PDF object:
-    file.close()
+
+
 
 
 if __name__ == "__main__":
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='CovidFL',
+    version='1.0.0a.1',
+    packages=['CovidFL'],
+    url='https://github.com/OEUG99/Florida-COVID19-Parser',
+    license='MIT',
+    author='O. Eugenio',
+    author_email='iam@oweneugenio.com',
+    description='A parser that extracts the Statistics from Florida\'s Department of Health\'s COVID-19 Weekly '
+                'Situtation Report. '
+)
